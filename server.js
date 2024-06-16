740347309b1f329303441c2bc02cb17d5459de29
JoE <156410520+narches@users.noreply.github.com>
2024-06-16 01:34:21 +0100

    Name Adjustment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send(" Joy Okah Ngozi");
+  res.send(" Hello World! I'm a Backend Developer now!");
 });
  
 const port = 3000;
